c39e4799cd31cec7b427d5b0dcd21fe52e20964d
Leonardo Cezar <paaarx@gmail.com>
2020-05-24 23:24:44 -0300

    Initial commit.

---
+++ get_nulls.py
@@ -0,0 +1,30 @@
+def get_nulls(df):
+    """
+    https://github.com/paaarx
+    Returns a DataFrame with null count.
+
+    For every column in DataFrame, analyzes each row to search for null values
+    and count how many are found.
+    Only columns with null values will be returned.
+
+    Parameters:
+        df (DataFrame): The DataFrame to be analyzed.
+
+    Returns:
+        DataFrame: DataFrame with columns and null count.
+    """
+
+    expression = []
+
+    for column in df.columns:
+        expression.append(count(when(isnull(column), column)).alias(column))
+
+    df_with_nulls = df.select(expression)
+
+    column_list = []
+
+    for key, value in df_with_nulls.collect()[0].asDict().items():
+        if value > 0:
+            column_list.append(key)
+
+    return df_with_nulls.select(column_list)
