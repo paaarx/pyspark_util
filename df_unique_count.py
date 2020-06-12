d2693a176d89b2bd4357f1bab535e34350d27fc1
Leonardo Cezar <paaarx@gmail.com>
2020-06-12 19:50:40 -0300

    changed def name and documentation

--- df_unique_count.py
+++ df_unique_count.py
@@ -3,12 +3,15 @@ from pyspark.sql.functions import count
 
 def unique_count(df):
     """
-    https://github.com/paaarx
     Returns a DataFrame with unique values count.
+
+    https://github.com/paaarx
     For every column in DataFrame, analyzes each row to search for unique
     values and count how many are found.
+
     Parameters:
         df (DataFrame): The DataFrame to be analyzed.
+
     Returns:
         DataFrame: DataFrame with columns and unique values count.
     """
